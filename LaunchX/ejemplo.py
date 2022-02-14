42d8b7b24d1c6326d4ffc03bc3218843402df659
Hazel Adrian Ponce Cervantes <99230313+HazelPonce@users.noreply.github.com>
2022-02-14 15:15:33 -0600

    Add: files from cloud gui

---
+++ LaunchX/ejemplo.py
@@ -0,0 +1,19 @@
+# ejemplo.py
+from datetime import date
+from unicodedata import name
+sum = 1 + 2
+print(sum)
+sum = 1 + 2 # 3
+product = sum * 2
+print(product)
+distancia_a_alfa_centauri = 4.56
+print(type(distancia_a_alfa_centauri))
+print(date.today())
+print("Today's date is: "+str(date.today()))
+print("Bienvenido al programa de bienvenida")
+name=input("Introduzca su nombre ")
+print("Saludos "+name)
+print("Calculadora")
+first_number = input("Primer numero: ")
+second_number = input("Segundo numero: ")
+print(int(first_number) + int(second_number))
---
+++ LaunchX/tempCodeRunnerFile.py
@@ -0,0 +1,7 @@
+sum = 1 + 2
+print(sum)
+sum = 1 + 2 # 3
+product = sum * 2
+print(product)
+distancia_a_alfa_centauri = 4
+type(distancia_a_alfa_centauri)
